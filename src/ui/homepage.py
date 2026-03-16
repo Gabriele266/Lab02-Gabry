c1655e5efc297587148ef46173e397401beb4c7e
Informatica.io <informaticaio.info@gmail.com>
2026-03-16 18:10:16 +0100

    Implement basic homepage with table showing all the translations

--- src/ui/homepage.py
+++ src/ui/homepage.py
@@ -1,6 +1,8 @@
 import flet as ft
 from src.ui.generic import PageClass
 from src.domain.classes import Dictionary
+from src.ui.settings import GeneralSettings
+
 
 class Homepage (PageClass):
     def __init__(self, dictionary: Dictionary):
@@ -8,8 +10,51 @@ class Homepage (PageClass):
         super().__init__()
 
     def present(self, page: ft.Page):
-        page.title = "Hello word application"
+        page.title = "Dictionary application - Gabry"
 
         page.add(
-            ft.Text(f"Loaded {self._dictionary.size()}"),
+            ft.Column(
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                controls=[
+                    ft.Text("Dictionary application", size=30, color=ft.Colors.WHITE),
+                    ft.Text(f"Loaded dictionary from file {GeneralSettings.__FILENAME__} with {self._dictionary.size()} translations", size=12, color=ft.Colors.GREY),
+                            ft.Container(
+                                border_radius=10,
+                                border=ft.Border.all(2, ft.Colors.WHITE),
+                                content=ft.Column(
+                                    height=600,
+                                    expand=True,
+                                    scroll=ft.ScrollMode.ALWAYS,
+                                    alignment=ft.MainAxisAlignment.CENTER,
+                                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                    controls=[ft.DataTable(
+                                        columns=[
+                                            ft.DataColumn(
+                                                label=ft.Text("Alien term")
+                                            ),
+                                            ft.DataColumn(
+                                                label=ft.Text("Italian translations")
+                                            ),
+                                            ft.DataColumn(
+                                                label=ft.Text("Actions")
+                                            )
+                                        ],
+                                    rows=self._format_rows()
+                            )]))
+                ]
+            )
         )
+
+    def _format_rows(self) -> list[ft.DataRow]:
+        rows = []
+
+        self._dictionary.map_dictionary(lambda translation: rows.append(ft.DataRow(
+            cells=[
+                ft.DataCell(ft.Text(translation.alien)),
+                ft.DataCell(ft.Text(translation.translate())),
+                ft.DataCell(ft.Row([ft.Button(icon=ft.Icons.EDIT, content="Edit"), ft.Button(icon=ft.Icons.DELETE, content="Delete")]))
+            ]
+        )))
+
+        return rows
